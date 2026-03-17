355434c9782dfa31f5c0b6f0d30a8731d923b095
Fábio Silva <fabio.silva12022001@gmail.com>
2026-03-16 21:22:17 -0300

    Average

---
+++ Training phyton/Average.py
@@ -0,0 +1,19 @@
+num1 = 5
+num2 = 20
+
+soma = num1 + num2 
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print(f"A soma é: {soma}")
+print(f"A multiplicacao é: {multiplicacao}")
+print(f"A divisao é: {divisao}")
+
+aluno = ("Ben")
+n1 = 2
+n2 = 10
+n3 = 6
+
+media = (n1 + n2 + n3) / 3
+
+print(f"Ben, sua média é {media}")
